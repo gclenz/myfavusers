3f502a6f82c30a854c7882f8a7738d70a2a75477
Gabriel Lenz <gabrielclenz@gmail.com>
2019-11-17 02:47:16 -0300

    first commit

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Form = styled.View`
+  border-bottom-color: #eee;
+  border-bottom-width: 3px;
+  flex-direction: row;
+  padding-bottom: 20px;
+`;
+
+export const Input = styled.TextInput`
+  background-color: #eee;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  flex: 1;
+  padding: 5px 10px;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  background-color: #a283e5;
+  border-radius: 5px;
+  justify-content: center;
+  margin-left: 10px;
+  padding: 0 10px;
+  opacity: ${props => (props.loading ? 0.6 : 1)};
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0px 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  background-color: #eee;
+  border-radius: 32px;
+  height: 64px;
+  width: 64px;
+`;
+
+export const Name = styled.Text`
+  color: #333;
+  font-size: 14px;
+  font-weight: bold;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  color: #999;
+  font-size: 13px;
+  line-height: 18px;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const ProfileButton = styled(RectButton)`
+  align-items: center;
+  align-self: stretch;
+  background-color: #a283e5;
+  border-radius: 5px;
+  height: 36px;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+export const ProfileButtonText = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
---
+++ src/pages/User/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Avatar = styled.Image`
+  background-color: #eee;
+  border-radius: 50px;
+  height: 100px;
+  width: 100px;
+`;
+
+export const Name = styled.Text`
+  color: #333;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 10px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text`
+  color: #999;
+  font-size: 14px;
+  line-height: 18px;
+  margin-top: 10px;
+  text-align: center;
+`;
+
+export const Stars = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Starred = styled.View`
+  align-items: center;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+  flex-direction: row;
+  margin-bottom: 10px;
+  padding: 10px 15px;
+`;
+
+export const OwnerAvatar = styled.Image`
+  background-color: #eee;
+  border-radius: 21px;
+  height: 42px;
+  width: 42px;
+`;
+
+export const Info = styled.View`
+  flex: 1;
+  margin-left: 10px;
+`;
+
+export const Title = styled.Text.attrs({
+  numberOfLines: 1,
+})`
+  color: #333;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const Author = styled.Text`
+  color: #555;
+  font-size: 13px;
+  margin-top: 2px;
+`;
